239ccb27574e4584f0db4ebdf93ce7283ed132b0
pattobrien <12474318+pattobrien@users.noreply.github.com>
2024-02-23 17:50:10 -0500

    feat: Working fs example.

--- build/main_with_preamble.js
+++ build/main_with_preamble.js
@@ -2,6 +2,11 @@
 (async () => {
     // runs preamble.js before main.js
   await import('./node_preamble.js');
+
+  // NOTE: explicit import of fs module and added it to global scope
+  const fs = await import('fs');
+  globalThis.fs = fs;
+
   // await import('./gen/console_interop.dart.js');
   await import('./gen/fs_interop.dart.js');
 
