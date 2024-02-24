034577dacf419509d818209f7aafea228f791bad
pattobrien <12474318+pattobrien@users.noreply.github.com>
2024-02-23 19:44:17 -0500

    docs: Added notes to fs_interop.

--- bin/fs_interop.dart
+++ bin/fs_interop.dart
@@ -6,9 +6,15 @@ import 'package:dart3_node_interop/dart3_node_interop.dart';
 
 /// Reads from the file system and prints the files in the given directory.
 ///
+/// This example requires the import of the `fs` module that is built into
+/// Node.js. To avoid requiring users of our `fs` module to explicitly import
+/// the `fs` module, we use a top level `FS` extension type that contains
+/// all of the properties contained in fs. The user must then await the top
+/// level `fs` getter in order to call the `readdirSync` method.
 ///
+/// NOTE: this dev x should improve following the completion of this issue:
+/// https://github.com/dart-lang/sdk/issues/53783
 Future<void> main() async {
-  // final module = await importModule('fs').toDart;
   final thisDir = '/Users/pattobrien/dev/pattobrien/dart3_node_interop';
   final files = (await fs).readdirSync(thisDir.toJS);
   for (final file in files.toDart) {
--- lib/types/fs.dart
+++ lib/types/fs.dart
@@ -3,43 +3,23 @@ library fs;
 
 import 'dart:js_interop';
 
-// Future<FS> get fs => importModule('fs').toDart.then(
-//       (value) => value.typeofEquals('fs')
-//           ? value as FS
-//           : throw Exception('Module fs not found'),
-//     );
 @JS()
 Future<FS> get fs async => FS._(await importModule('fs').toDart);
 
-// @JS()
 extension type FS._(JSObject _) implements JSObject {
-  // @JS('readFileSync')
   external JSAny readFileSync(
     JSAny file, [
     JSAny? options,
   ]);
-  // @JS('writeFileSync')
+
   external void writeFileSync(
     JSAny file,
     JSAny data, [
     JSAny? options,
   ]);
-  // @JS('readdirSync')
+
   external JSArray<JSString> readdirSync(
     JSAny path, [
     JSAny? options,
   ]);
 }
-
-// @JS()
-// external void writeFileSync(
-//   JSAny file,
-//   JSAny data, [
-//   JSAny? options,
-// ]);
-
-// @JS()
-// external JSArray<JSString> readdirSync(
-//   JSAny path, [
-//   JSAny? options,
-// ]);
